4f60dbb3a4f3b6eb692763866699919ef8eb4e1e
Antho1721 <letellieranthony17@hotmail.fr>
2023-06-29 19:37:03 +0200

    bdd start and mvc model on php

---
+++ model/customerReview.php
@@ -0,0 +1,34 @@
+<?php
+
+function getCustomerReview() {
+
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=garage_parrot', 'root', '');
+
+        $statement = $database->query('SELECT 
+    customer_review_username, 
+    customer_review_date, 
+    customer_review_note, 
+    customer_review_text 
+FROM customer_review;');
+
+        $reviews = [];
+        while($review = $statement->fetch(PDO::FETCH_ASSOC)){
+            $userReview =['author' => $review['customer_review_username'],
+                'date' => $review['customer_review_date'],
+                'note' => $review['customer_review_note'],
+                'comment' => $review['customer_review_text'],
+            ];
+            $reviews[] = $userReview;
+        }
+
+        return $reviews;
+
+    } catch(Exception $e){
+
+        echo $e->getMessage();
+
+    }
+
+
+}
