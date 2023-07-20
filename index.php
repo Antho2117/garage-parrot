c595ad99398fef85a12d04531b46a3038e956bfd
Antho1721 <letellieranthony17@hotmail.fr>
2023-07-20 09:58:03 +0200

    deploy

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Autoloader;
+
 // On définit une constante contenant le dossier racine
 const ROOT = "/gvp";
 const BASE_UPLOADS = "/Applications/MAMP/htdocs".ROOT."/uploads/";
 // On importe les namespaces nécessaires
-use App\Autoloader;
-use App\Core\Main;
 
 // On importe l'Autoloader
 require_once 'Autoloader.php';
 Autoloader::register();
 
 // On instancie Main
-$app = new Main();
+$app = new Main;
 
 // On démarre l'application
 $app->start();
